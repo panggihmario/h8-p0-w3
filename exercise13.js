dfc8ffde2688902a12a8c646f0ef1f12c16102a9
mario <panggihmario@gmail.com>
2018-04-07 16:21:18 +0700

    up

--- exercise13.js
+++ exercise13.js
@@ -1,43 +1,48 @@
 function targetTerdekat(arr) {
-  var untukX=0
-  var untukO=0
-  var indeks=[]
-  var indek=[]
-  var result=0
-   if(arr.indexOf('x') === -1){
-    return 0}
-    else if(arr.indexOf('o')===-1){
-    return 0}
-  else{
+  // you can only write your code here!
+   
+  var kurang=0
+  if(arr.indexOf('x') ===-1){
+    return 0
+  }
+  else if(arr.indexOf('o') ===-1){
+    return 0
+  }
+  var tampungX=[]
+  var tampungO=[]
   for(var i=0;i<arr.length;i++){
-    if(arr[i]==='x'){
-      untukX=i
-      indeks.push(untukX)
-    }
-    else if(arr[i]==='o'){
-      untukO=i
-      indek.push(untukO)
-    }
-    var hasil=[]
-    for(var j=0;j<indeks.length;j++){
-      if(untukX>untukO){
-      result=Math.abs(untukO-indeks[j])
-      hasil.push(result)
-      hasil.sort()}
-      else if(untukX<untukO){
-        result=Math.abs(untukX-indek[j])
-      hasil.push(result)
-      hasil.sort()}
+      if(arr[i]==='x'){
+        tampungX.push(i)
       }
+      else if(arr[i]==='o'){
+        tampungO.push(i)
+      }
+  }
+  var tampung=0
+  var hasil=[]
+  for(var j=0;j<tampungX.length;j++){
+    for(var k=0;k<=tampungO.length;k++){
+    if(tampungX[j]>tampungO[k]){
+    tampung=tampungX[j]-tampungO[k]
+    hasil.push(tampung)
+    }
+    else if(tampungX[j]<tampungO[k]){
+      tampung=tampungO[k]-tampungX[j]
+      hasil.push(tampung)
     }
   }
-   return hasil[0]
-}
-// TEST CASES
-console.log(targetTerdekat(['', '', 'x', '', '', 'o', '', 'o'])); // 3
-console.log(targetTerdekat(['o', '', '', '', 'x', 'x', 'x'])); // 4
-console.log(targetTerdekat(['x', '', '', '', 'x', 'x', 'o', ''])); // 1
-console.log(targetTerdekat(['', '', 'o', ''])); // 0
-console.log(targetTerdekat(['', 'o', '', 'x', 'x', '', '', 'x'])); // 2
+  }
+var final=hasil.sort()
+return final[0]
 
-//
+
+
+
+}
+
+// TEST CASES
+console.log(targetTerdekat([' ', ' ', 'o', ' ', ' ', 'x', ' ', 'x'])); // 3
+console.log(targetTerdekat(['o', ' ', ' ', ' ', 'x', 'x', 'x'])); // 4
+console.log(targetTerdekat(['x', ' ', ' ', ' ', 'x', 'x', 'o', ' '])); // 1
+console.log(targetTerdekat([' ', ' ', 'o', ' '])); // 0
+console.log(targetTerdekat([' ', 'o', ' ', 'x', 'x', ' ', ' ', 'x'])); // 2
